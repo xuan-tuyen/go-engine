b3133aea39473961d282fde3eef5a1ad6cabe0b6
esrrhs <esrrhs@163.com>
2019-10-26 18:03:12 +0800

    add
    Former-commit-id: 60879bdff043b8108619f948982adf18e6b16794

--- src/common/string.go
+++ src/common/string.go
@@ -8,3 +8,11 @@ import (
 func IntArrayToString(a []int, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
+
+func Int32ArrayToString(a []int32, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
+
+func Int64ArrayToString(a []int64, delim string) string {
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+}
