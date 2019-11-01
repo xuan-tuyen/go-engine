153805938a130e0749aa63ce2c3ada2ead8faabc
esrrhs <esrrhs@163.com>
2019-11-01 23:42:08 +0800

    add

--- src/common/string.go
+++ src/common/string.go
@@ -1,18 +1,29 @@
 package common
 
 import (
-	"fmt"
-	"strings"
+	"strconv"
 )
 
 func IntArrayToString(a []int, delim string) string {
-	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+	var ret string
+	for _, s := range a {
+		ret += strconv.Itoa(s) + delim
+	}
+	return ret
 }
 
 func Int32ArrayToString(a []int32, delim string) string {
-	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+	var ret string
+	for _, s := range a {
+		ret += strconv.Itoa((int)(s)) + delim
+	}
+	return ret
 }
 
 func Int64ArrayToString(a []int64, delim string) string {
-	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+	var ret string
+	for _, s := range a {
+		ret += strconv.Itoa((int)(s)) + delim
+	}
+	return ret
 }
