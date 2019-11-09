2bcb0da204dab601be331acda27fdff2426ade6a
esrrhs <esrrhs@163.com>
2019-11-09 21:32:09 +0800

    add
    Former-commit-id: ce4970c7f7488403ac8859d5c549fd6e643627d7

--- src/rudp/client_test.go
+++ src/rudp/client_test.go
@@ -3,14 +3,33 @@ package rudp
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test0001(t *testing.T) {
 
-	_, err := Dail("192.168.1.1:8888", 1000, nil)
+	lis, err := Listen("127.0.0.1:9999", nil)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	conn, err := Dail("127.0.0.1:9999", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	c := lis.Accept(100)
+	fmt.Println(c)
+
+	c.Write([]byte("123123123123"))
+	time.Sleep(time.Second)
+	str, err := conn.Read()
+	fmt.Println(string(str))
+
+	lis.Close()
+	conn.Close()
 	fmt.Println("done ")
 
 }
