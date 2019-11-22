271d6247f792b2aabff0a11d2f3ec42f373d6fa6
esrrhs <esrrhs@163.com>
2019-11-22 09:28:18 +0800

    add
    Former-commit-id: 9d0359b2d84f050bbdd173ccf959d3ad0837424c

--- src/console/input_linux.go
+++ src/console/input_linux.go
@@ -1,1 +1,15 @@
 package console
+
+type ConsoleInput struct {
+}
+
+func (ci *ConsoleInput) Init() error {
+	return nil
+}
+
+func (ci *ConsoleInput) Close() {
+}
+
+func (ci *ConsoleInput) PollEvent() *EventKey {
+	return nil
+}
