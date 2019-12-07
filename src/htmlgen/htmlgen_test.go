0bebd31c257b75092169db04e27ae59512fb34d9
esrrhs <esrrhs@163.com>
2019-12-07 20:38:24 +0800

    add

--- src/htmlgen/htmlgen_test.go
+++ src/htmlgen/htmlgen_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test0001(t *testing.T) {
 	common.Ini()
-	hg := New("test", "./", 10, 10, "", "")
+	hg := New("test", "./", 10, 10, "./mainpage.tpl", "./subpage.tpl")
 
 	hg.AddHtml("aaa")
 	hg.AddHtml("啊啊")
