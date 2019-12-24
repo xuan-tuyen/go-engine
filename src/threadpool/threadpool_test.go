d430e1fb922027617f93503b6933089c776dee03
esrrhs <esrrhs@163.com>
2019-12-24 09:25:24 +0800

    add
    Former-commit-id: ee309af91f4b0736c7ce2faa84b84caec53e1f9c

--- src/threadpool/threadpool_test.go
+++ src/threadpool/threadpool_test.go
@@ -3,6 +3,7 @@ package threadpool
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -16,6 +17,23 @@ func TestNew(t *testing.T) {
 	tp.AddJob(3, 3)
 	tp.AddJob(4, 4)
 	tp.AddJob(201, 201)
+	tp.Stop()
+	fmt.Println("Stop")
+}
+
+func Test2(t *testing.T) {
+	tp := NewThreadPool(2, 1, func(i interface{}) {
+		v := i.(int)
+		fmt.Println(v)
+		time.Sleep(time.Second)
+	})
+	tp.AddJob(0, 0)
+	tp.AddJob(1, 1)
+	tp.AddJob(2, 2)
+	tp.AddJob(3, 3)
+	tp.AddJob(4, 4)
+	tp.AddJob(5, 5)
+	time.Sleep(time.Second * 2)
 	tp.Stop()
 	fmt.Println("Stop")
 }
