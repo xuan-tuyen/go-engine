5097c2705e38921ab91417aa4ccd5001265bb77b
esrrhs <esrrhs@163.com>
2019-12-27 18:51:34 +0800

    add

--- src/fifo/fifio_test.go
+++ src/fifo/fifio_test.go
@@ -2,7 +2,9 @@ package fifo
 
 import (
 	"fmt"
+	"github.com/esrrhs/go-engine/src/common"
 	"testing"
+	"time"
 )
 
 func Test0001(t *testing.T) {
@@ -16,4 +18,41 @@ func Test0001(t *testing.T) {
 	for f.GetSize() > 0 {
 		fmt.Println(f.Read())
 	}
+}
+
+func Test0002(t *testing.T) {
+
+	f := NewFIFO("aa")
+
+	go func() {
+		last := time.Now()
+		n := 0
+		for {
+			f.Write(common.RandStr(10))
+			n++
+			if time.Now().Sub(last) > time.Second {
+				fmt.Printf("write %d\n", n)
+				n = 0
+				last = time.Now()
+			}
+		}
+	}()
+
+	go func() {
+		last := time.Now()
+		n := 0
+		for {
+			_, err := f.Read()
+			if err == nil {
+				n++
+			}
+			if time.Now().Sub(last) > time.Second {
+				fmt.Printf("read %d\n", n)
+				n = 0
+				last = time.Now()
+			}
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
 }
