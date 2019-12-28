778ac1d6bc7ffaacdec38ed4bae567876ee5a852
esrrhs <esrrhs@163.com>
2019-12-28 08:06:31 +0800

    add
    Former-commit-id: a73902dce47b8578f3b3a1a49ef265b715073376

--- src/fifo/fifio_test.go
+++ src/fifo/fifio_test.go
@@ -2,18 +2,11 @@ package fifo
 
 import (
 	"fmt"
-	"github.com/esrrhs/go-engine/src/common"
 	"testing"
-	"time"
 )
 
 func Test0001(t *testing.T) {
-	f, err := NewFIFO("aa")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
+	f, _ := NewFIFO("aa")
 	f.Write("aa")
 	f.Write("bb")
 	f.Write("cc")
@@ -23,45 +16,4 @@ func Test0001(t *testing.T) {
 	for f.GetSize() > 0 {
 		fmt.Println(f.Read())
 	}
-}
-
-func Test0002(t *testing.T) {
-
-	f, err := NewFIFO("aa")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	go func() {
-		last := time.Now()
-		n := 0
-		for {
-			f.Write(common.RandStr(10))
-			n++
-			if time.Now().Sub(last) > time.Second {
-				fmt.Printf("write %d\n", n)
-				n = 0
-				last = time.Now()
-			}
-		}
-	}()
-
-	go func() {
-		last := time.Now()
-		n := 0
-		for {
-			_, err := f.Read()
-			if err == nil {
-				n++
-			}
-			if time.Now().Sub(last) > time.Second {
-				fmt.Printf("read %d\n", n)
-				n = 0
-				last = time.Now()
-			}
-		}
-	}()
-
-	time.Sleep(10 * time.Second)
 }
