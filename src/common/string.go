7c4d1967401b9346dff47acd0c72624055550998
esrrhs <esrrhs@163.com>
2019-12-31 11:02:01 +0800

    add

--- src/common/string.go
+++ src/common/string.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"strconv"
+	"strings"
 )
 
 const gcharset = "abcdefghijklmnopqrstuvwxyz" +
@@ -45,4 +46,89 @@ func RandStr(l int) string {
 		b[i] = gcharset[RandInt31n(len(gcharset))]
 	}
 	return string(b)
+}
+
+type StrTableLine struct {
+	cols []string
+}
+
+type StrTable struct {
+	header []string
+	lines  []StrTableLine
+}
+
+func (s *StrTable) AddHeader(h string) {
+	s.header = append(s.header, h)
+}
+
+func (s *StrTable) AddLine(l StrTableLine) {
+	s.lines = append(s.lines, l)
+}
+
+func (s *StrTableLine) AddData(d string) {
+	s.cols = append(s.cols, d)
+}
+
+func (s *StrTable) String() string {
+
+	if len(s.header) <= 0 {
+		return ""
+	}
+
+	colmax := make([]int, 0)
+	for _, s := range s.header {
+		colmax = append(colmax, len(s))
+	}
+
+	totalcol := 0
+	for i := 0; i < len(colmax); i++ {
+		max := colmax[i]
+		for _, sl := range s.lines {
+			if i < len(sl.cols) {
+				max = MaxOfInt(max, len(sl.cols[i]))
+			}
+		}
+		colmax[i] = max
+		totalcol += max
+	}
+	totalcol += len(colmax) + 1
+
+	/*
+		-----------
+		| a  | b  |
+		-----------
+		| 1  | 2  |
+		-----------
+	*/
+
+	ret := ""
+	ret += strings.Repeat("-", totalcol) + "\n"
+	for i, h := range s.header {
+		ret += "|" + WrapString(h, colmax[i])
+	}
+	ret += "|" + "\n"
+
+	for _, l := range s.lines {
+		ret += strings.Repeat("-", totalcol) + "\n"
+		for i, d := range l.cols {
+			ret += "|" + WrapString(d, colmax[i])
+		}
+		for i := len(l.cols); i < len(colmax); i++ {
+			ret += "|" + WrapString("", colmax[i])
+		}
+		ret += "|" + "\n"
+	}
+
+	ret += strings.Repeat("-", totalcol) + "\n"
+
+	return ret
+}
+
+func WrapString(s string, n int) string {
+	if n <= len(s) {
+		return s
+	}
+	l := (n - len(s)) / 2
+	r := (n - len(s)) - l
+	return strings.Repeat(" ", l) + s + strings.Repeat(" ", r)
 }
