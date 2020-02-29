6c42c2f7f370280578b05c9ba80f9bf76963fd84
esrrhs <esrrhs@163.com>
2020-02-29 23:44:57 +0800

    add
    Former-commit-id: 157c8a1f15194e6d57566b21a98ab1bf6026b792

--- src/pingtunnel/server.go
+++ src/pingtunnel/server.go
@@ -182,11 +182,6 @@ func (p *Server) processDataPacketNewConn(id string, packet *Packet) *ServerConn
 
 	if packet.my.Tcpmode > 0 {
 
-		if packet.my.TcpmodeBuffersize <= 0 {
-			loggo.Info("tcpmod buffsize error, maybe old conn %s %s", id, addr)
-			return nil
-		}
-
 		c, err := net.DialTimeout("tcp", addr, time.Millisecond*time.Duration(p.connecttmeout))
 		if err != nil {
 			loggo.Error("Error listening for tcp packets: %s %s", id, err.Error())
