ac6007e0f4efb5a7ccac497d7c2fcd0c47461973
esrrhs <esrrhs@163.com>
2020-06-16 08:51:22 +0800

    add

--- src/proxy/common.go
+++ src/proxy/common.go
@@ -35,8 +35,8 @@ type Config struct {
 func DefaultConfig() *Config {
 	return &Config{
 		MaxMsgSize:         1024 * 1024,
-		MainBuffer:         1024 * 1024,
-		ConnBuffer:         1024,
+		MainBuffer:         1024,
+		ConnBuffer:         128,
 		EstablishedTimeout: 10,
 		PingInter:          1,
 		PingTimeoutInter:   5,
